2cccd5b0d0eeb3c4b228a2af283e240f2293cd86
Click1N <111238061+Click1N@users.noreply.github.com>
2022-10-02 17:50:41 +0200

    settings for first view with request and response function has been update with name_get input

--- apps/base/views.py
+++ apps/base/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
+from apps.base.servicies.get_random_name import faker_name
+
+
+def say_hi(request: HttpRequest, name=None) -> HttpResponse:
+    if name is None:
+        name = faker_name()
+    return HttpResponse(f"Hello {name}, welcome")
