f2863dde11b9b4e955f479bf0653883fbb844178
Click1N <111238061+Click1N@users.noreply.github.com>
2022-10-04 13:16:16 +0200

    add firs app : base (test app)

--- apps/base/urls.py
+++ apps/base/urls.py
@@ -5,3 +5,12 @@ urlpatterns = [
     path("", views.say_hi, name="say_hi"),
 ]
 # here we call function as an object, we give name for this path
+
+# urlpatterns = [
+#    path("", views.index, name="index"),
+# ]
+# path for index view
+
+# urlpatterns = [
+#    path("", views.random_mes, name="random_mes"),
+# ]
--- apps/base/views.py
+++ apps/base/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
 
 from apps.base.servicies.get_random_name import faker_name
 
@@ -7,3 +8,11 @@ def say_hi(request: HttpRequest, name=None) -> HttpResponse:
     if name is None:
         name = faker_name()
     return HttpResponse(f"Hello {name}, welcome")
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    return render(request, "index.html")
+
+
+def random_mes(request: HttpRequest) -> HttpResponse:
+    return render(request, "random_message.html", {"random_message": faker_name()})
