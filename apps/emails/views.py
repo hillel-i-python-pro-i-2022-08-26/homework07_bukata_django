fcab15d8d48d7ed96222bd95f7b60dab2a591645
Click1N <111238061+Click1N@users.noreply.github.com>
2022-10-04 13:22:54 +0200

    emails app creation add services add view add urls
    app and url linked to core and templates
    local template has been created
    
    app has been ad to local apps (core settings)

---
+++ apps/emails/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+from apps.emails.servicies.get_random_email import faker_email
+
+
+def random_email(request: HttpRequest) -> HttpResponse:
+    return render(request, "random_email.html", {"random_email": faker_email()})
